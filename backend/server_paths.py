36624b6ecbd00e16fc371744d59904b6a1225c67
Ant13731 <hunt.ant137@gmail.com>
2024-08-02 23:35:06 -0400

    move react to its own folder

--- backend/server_paths.py
+++ backend/server_paths.py
@@ -3,5 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/test")
+def test():
+    return {"test": "test"}
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5353, debug=True)
